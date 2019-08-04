4af03597fbced9ad168baa290f4ba7ed61c842c5
Ian Tatiana <ian.tatiana.v@gmail.com>
2019-08-04 23:22:22 +1000

    Add script for test detector

---
+++ dlib_detector/test.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import glob
+
+import numpy as np
+import cv2
+import dlib
+
+
+ddir = "./detector"
+
+def processFile(path, detector):
+    for f in glob.glob(os.path.join(path, "*.png")):
+        img = cv2.imread(f)
+        print(f"Processing file: {f}")
+        dets = detector(img)
+        print(f"Number of coins detected: {len(dets)}")
+        for k, d in enumerate(dets):
+            print(f"Detection {k}: Left: {d.left()} Top: {d.top()} Right: {d.right()} Bottom: {d.bottom()}")
+            cv2.rectangle(img, (d.left(), d.top()), (d.right(), d.bottom()), (0, 128, 255))
+
+        cv2.imshow("Output", img)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+
+def main():
+    fpath = "C:/Projects/data/coins/frames"
+    accordanceDict = dict()
+
+    for coinClassName in os.listdir(fpath):
+        for i in range(1, 4):
+            accordanceDict[f"{os.path.join(fpath, coinClassName, str(i))}"] = dlib.simple_object_detector(
+                os.path.join(ddir, f"detector_{coinClassName}_{i}.svm"))
+
+    for path, detector in accordanceDict.items():
+        processFile(path, detector)
+
+
+if __name__ == "__main__":
+    main()
