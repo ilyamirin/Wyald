70e32b17596bff708d8a6828e3f072e785bdef7a
Olga <rammsteinlifada@mail.ru>
2019-08-21 16:38:05 +1000

    bla bla bla

--- framing.py
+++ framing.py
@@ -2,13 +2,14 @@ import os
 import imageio
 import imgaug as ia
 from imgaug import augmenters as iaa
+from imgaug.augmentables.bbs import BoundingBox, BoundingBoxesOnImage
 
 import cv2
 import json
 
 lastIdx = dict()
 
-def augmentImage(image, bbox):
+def augmentImage(image, x1, x2, y1, y2):
     images = [ image for i in range(0, 5)]
     seq = iaa.Sequential([
         iaa.AdditiveGaussianNoise(scale=(10, 60)),
@@ -16,10 +17,10 @@ def augmentImage(image, bbox):
     ], random_order=True)
 
     bbs = BoundingBoxesOnImage([
-        BoundingBox(x1=0.2 * 447, x2=0.85 * 447, y1=0.3 * 298, y2=0.95 * 298),
-        BoundingBox(x1=0.4 * 447, x2=0.65 * 447, y1=0.1 * 298, y2=0.4 * 298)
+        BoundingBox(x1=x1, x2=x2, y1=y1, y2=y2),
     ], shape=image.shape)
 
+    
     return images
 
 def framingVideoFile(rootDir, frameDir, vname):
@@ -30,6 +31,8 @@ def framingVideoFile(rootDir, frameDir, vname):
     if not os.path.exists(frameDstPath):
         os.makedirs(frameDstPath)
 
+    jsonFin = open(os.path.join(frameDir, vname[:-4], 'mark.json'), 'r')
+    jsonData = json.load(jsonFin)
     cap = cv2.VideoCapture(os.path.join(rootDir, vname))
     # idx = 0
     while cap.isOpened():
@@ -47,8 +50,10 @@ def framingVideoFile(rootDir, frameDir, vname):
         t = os.path.join(frameDstPath, fname)
         print(t)
         if not os.path.exists(t):
-            images = augmentImage(frame, )
+            y1, x1, y2, x2 = jsonData[os.path.join(frameDir, vname[:-4], fname)]
+            images = augmentImage(frame, x1, x2, y1, y2)
             cv2.imwrite(t, frame)
+    jsonFin.close()
 
 
 def main():
@@ -57,12 +62,8 @@ def main():
     frameDir = os.path.join(rootDir, 'frames')
     videoDir = os.path.join(rootDir, 'video')
 
-    # jsonMetaData = {}
-    #
-    # infoOutput = open('info.txt', "w")
     for videoFile in os.listdir(videoDir):
         framingVideoFile(videoDir, frameDir, videoFile)
-        # json.dump(f"{videoFile}: {{ 'frames': {framesCount}, 'completed': {True} }}", infoOutput, indent=3)
 
 if __name__ == "__main__":
     main()
