47aaefc231a64aebaea0ff923e362f9f1bede1c3
Ian V. Tatiana <ian.tatiana.v@gmail.com>
2019-08-23 00:34:01 +1000

    Update xml2json

--- xml2json.py
+++ xml2json.py
@@ -3,30 +3,34 @@ import sys
 import glob
 
 import xmltodict, json
-from lxml import etree
 import xmlschema
+from colorama import Fore, Back, Style
 
 categoryList = dict()
 
-def xml2json(path, filename, data):
-
-    if not xmlschema.validate(data):
-        print(f"{filename} is invalid")
+def xml2json(path, filename, xmlPath):
+    o = {}
+    try:
+        file = open(os.path.join(xmlPath), "r")
+        data = file.read()
+        o = xmltodict.parse(data)
+    except:
+        print(f"{Fore.RED} Couldn't parse {xmlPath}")
         return {}
 
-    o = xmltodict.parse(data)
     imgList = o['dataset']["images"]["image"]
 
     jsonData = {}
     for image in imgList:
         if not "@frame" in image:
+            print(f"{Fore.RED} {filename} : The attribute '@frame' was not found")
             continue
+
         imgIdx = image['@frame']
         x1 = int(image['box']['@left'])
         y1 = int(image['box']['@top'])
         x2 = x1 + int(image['box']['@width'])
         y2 = y1 + int(image['box']['@height'])
-
 
         subCategory = f"-{image['@category']}" if '@category' in image else ""
         if subCategory != "":
@@ -34,56 +38,82 @@ def xml2json(path, filename, data):
 
         category = f"{filename[:-11]}{subCategory}"
         category = category.replace("_", "-")
-        jsonData[os.path.join(path, filename[:-8], f"{filename[:-8]}-{imgIdx}.jpg")] = {  # cut 9 symbols '.MOV.xml'
+        jsonData[os.path.join(path, filename[:-8], f"{filename[:-8]}-{imgIdx}.jpg")] = {  # cut 8 symbols '.MOV.xml'
             'category': category,  # cut 7 symbols '-v1.MOV.xml' or '-v{n}.xml'
             'coords': [y1, x1, y2, x2]
         }
 
         if not category in categoryList:
             count = len(categoryList)
-            categoryList[category] = count
+            categoryList[category] = {
+                "categoryIndex": count,
+                "name": category.replace("_", "-")
+            }
 
     return jsonData
 
 
-def tryCreateDirectory(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
+def checkDir(path, toCreate=False):
+    if os.path.exists(path):
         return path
+    if toCreate:
+        os.mkdir(path)
+        print(f"{Fore.GREEN} The directory {path} was create")
+        return path
+    print(f"{Fore.RED} The directory {path} is not found...")
+    exit(1)
 
-    return None # todo rm ?
+
+def tryLoadJsonData(pathToFile):
+    if not os.path.exists(pathToFile):
+        return {}
+    return json.load(open(pathToFile, 'r'))
 
 
 def main():
-    rootDir = r"E:\data\coins"
+    rootDir = r"D:\Projects\coins-project\data"
 
-    xmlDir = os.path.join(rootDir, 'xml')
-    frameDir = os.path.join(rootDir, 'frames')
-    videoDir = os.path.join(rootDir, 'sber', 'video')
+    xmlDir = checkDir(os.path.join(rootDir, 'xml'))
+    frameDir = checkDir(os.path.join(rootDir, 'frames'), toCreate=True)
+    coinsDir = checkDir(os.path.join(rootDir, 'sber'))
+    videoDir = checkDir(os.path.join(coinsDir, 'video'))
+
+    fileProcessDataPath = os.path.join(rootDir, 'process-data.txt')
+    jProcessData = tryLoadJsonData(fileProcessDataPath)
+    foutProcess = open(fileProcessDataPath, 'w')
 
     for dir in os.listdir(xmlDir):
-        for filename in os.listdir(os.path.join(xmlDir, dir)):
-            videoPath = os.path.join(videoDir, filename[:-4])
+        for filename in os.listdir(os.path.join(xmlDir, dir)): # filename ends '.MOV.xml'
+            jpd = {}
+
+            videoPath = os.path.join(videoDir, filename[:-4]) # cut 4 symbols '.xml'
             if not os.path.exists(videoPath):
+                print(f"{Fore.RED} Videofile {videoPath} is not found")
                 continue
 
             path = os.path.join(xmlDir, dir, filename)
-            file = open(os.path.join(path), "r")
-            print(path)
-            jsonData = xml2json(frameDir, filename, file.read())
+            jpd['video'] = videoPath
+            jpd['path'] = path
+            print(Style.RESET_ALL)
+            print(f"Start process file {path}")
+            jsonData = xml2json(frameDir, filename, path)
+
             if jsonData == {}:
+                print(f"{Fore.RED} The file {filename} is invalid")
                 continue
-            coinDir = os.path.join(frameDir, filename[:-8])
-            if not os.path.exists(coinDir):
-                os.mkdir(coinDir)
-            # json.dump(jsonData, open(os.path.join(coinDir, 'mark.json'), 'w'), indent=3)
+
+            coinDir = checkDir(os.path.join(frameDir, filename[:-8]), toCreate=True) # cur 8 symbols '.MOV.xml'
+            markJsonPath = os.path.join(coinDir, 'mark.json')
+            json.dump(jsonData, open(markJsonPath, 'w'), indent=3)
+            jpd['jsonPath'] = markJsonPath
+
+            jProcessData[filename] = jpd
 
     f = open(os.path.join(rootDir, 'categories.txt'), 'w')
     json.dump(categoryList, f, indent=3)
-    for val in categoryList:
-        f.writelines(f'\n"{val}" : "{val.replace("_", "-")}",')
-    for val in categoryList:
-        f.writelines(f"\n{val}")
+    json.dump(jProcessData, foutProcess, indent=3)
+    json.dump(jProcessData, foutProcess, indent=3)
+    foutProcess.close()
 
 
 if __name__ == "__main__":
