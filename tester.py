b1da9fb0dd038452f8a93d671349d2c0cdb908f3
Ian V. Tatiana <ian.tatiana.v@gmail.com>
2019-08-26 22:21:35 +1000

    Add tester

---
+++ tester.py
@@ -0,0 +1,78 @@
+from xml2json import xml2jsonFromFolder
+from verifier import crossMatchVideoAndMarks, actualizeInfoWithJsons, actualizeInfoWithFrames
+from framing import processVideoFolder
+from augmentation import augmentDatasetWithGenerator
+from darknet_preparation import cleanOldMarks, extractMarksThroughDataset, makeSets
+from config import Path, Extensions, Constants as const
+import augmentations_kit as ak
+
+def test():
+    try:
+        actualizeInfoWithFrames(Path.dataset)
+    except:
+        pass
+
+    xml2jsonFromFolder(
+        rpath=Path.rawXml,
+        wpath=Path.rawJson
+    )
+
+    crossMatchVideoAndMarks(
+        marks=Path.rawJson,
+        videos=Path.rawVideos
+    )
+
+    processVideoFolder(
+        folderPath=Path.rawVideos,
+        marksPath=Path.rawJson,
+        datasetPath=Path.dataset,
+        overwrite=False
+    )
+
+    # actualizeInfoWithFrames(Path.dataset)
+
+    augmentations = ak.cartoonAugs
+
+    augmentDatasetWithGenerator(
+        augmentationName="augmented",
+        augmentations=augmentations,
+        imageExtension=Extensions.png,
+        multiplier=2,
+        parallel=True
+    )
+
+    print()
+    actualizeInfoWithFrames(Path.dataset)
+    #
+
+    extractMarksThroughDataset(Path.dataset)
+    makeSets([Path.dataset])
+
+
+def main():
+
+    try:
+        actualizeInfoWithFrames(Path.dataset)
+    except:
+        pass
+
+    # import os
+    #
+    # augmentations = ak.cartoonAugs
+    #
+    # augmentDatasetWithGenerator(
+    #     augmentationName="hsl_yuv",
+    #     augmentations=augmentations,
+    #     imageExtension=Extensions.png,
+    #     multiplier=2,
+    #     parallel=True
+    # )
+    #
+    # extractMarksThroughDataset(os.path.join(Path.dataset, "hsl_yuv"))
+    # makeSets([
+    #     os.path.join(Path.dataset, "hsl_yuv")
+    # ])
+
+
+if __name__ == "__main__":
+    main()
