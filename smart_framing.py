0d9a5e45a18bc50764f1ea30286095d3de74160a
Anton <drobyshev.anton@ashmanov.net>
2019-08-30 20:52:12 +0300

    Мультипроцессорная функция для нарезки нескольких категорий

--- smart_framing.py
+++ smart_framing.py
@@ -12,7 +12,6 @@ from utils import makeJSONname, openJsonSafely, extendName
 from config import Extensions, Path, Constants as const
 
 
-
 def createGenerator(videosPath, videosInfo, overall, limit):
     pointer = 0
     step = ceil(overall / limit)
@@ -49,8 +48,9 @@ def augment():
     pass
 
 
-def extract(ctg, ctgInfo, videosPath:Path.rawVideos, extractionPath=Path.original, extension=Extensions.jpg, limit=None,
-            augmentFunc=None, augmentations=None, augmentationName=const.augmented, augmentationPath=None):
+def extract(ctg, ctgInfo, videosPath=Path.rawVideos, extractionPath=Path.original, extension=Extensions.jpg, limit=None,
+            augmentFunc=None, augmentations=None, augmentationName=const.augmented, augmentationPath=None,
+            overwriteOriginal=False, overwriteAugmented=True):
 
     parent = ctgInfo.get(const.parent, "")
     fullExtractionPath = os.path.join(extractionPath, parent, ctg)
@@ -77,6 +77,12 @@ def extract(ctg, ctgInfo, videosPath:Path.rawVideos, extractionPath=Path.origina
         augMarks = {}
         totalAugs = 0
 
+    fullCategory = getFullCategory(parent, ctg)
+    print("Cutting videos: \t {:>50} \t expected orig frames {:>10} \t expected aug frames {:>10} process id: {:>10}".
+          format(fullCategory, min(limit, overall), augmentations, os.getpid()))
+
+    # time.sleep(0.5)
+
     generator = createGenerator(videosPath, videos, overall, limit)
 
     marks = {}
@@ -84,32 +90,38 @@ def extract(ctg, ctgInfo, videosPath:Path.rawVideos, extractionPath=Path.origina
     for idx, genInfo in enumerate(generator):
         frame, frameName, coords = genInfo
 
-        fullCategory = getFullCategory(parent, ctg)
         fullFrameName = const.separator.join((fullCategory, frameName, const.original))
+        framePath = os.path.join(fullExtractionPath, const.frames, extendName(fullFrameName, extension))
 
         coords = fitCoords(coords, frame.shape[:2])
 
-        frameMarks = {
-            const.image: extendName(fullFrameName, extension),
-            const.coords: coords,
-            const.fullCategory: fullCategory,
-            const.ctgIdx: ctgInfo[const.ctgIdx],
-            const.imageShape: frame.shape[:2]
-        }
+        if not os.path.exists(framePath) or overwriteOriginal:
+            frameMarks = {
+                const.image: extendName(fullFrameName, extension),
+                const.coords: coords,
+                const.fullCategory: fullCategory,
+                const.ctgIdx: ctgInfo[const.ctgIdx],
+                const.imageShape: frame.shape[:2]
+            }
 
-        cv2.imwrite(os.path.join(fullExtractionPath, const.frames, extendName(fullFrameName, extension)), frame)
-        marks[frameName] = frameMarks
+            cv2.imwrite(framePath, frame)
+            marks[frameName] = frameMarks
 
-        total += 1
+            total += 1
 
         if augmentFunc is not None:
             frameAugments = 0
             for i in range(augRepeats):
+                augFrameName = f"{fullCategory}{const.separator}{frameName}_{i}{const.separator}{augmentationName}"
+                augFramePath = os.path.join(fullAugmentationPath, const.frames, extendName(augFrameName, extension))
+
                 if totalAugs == augmentations:
                     break
 
+                if os.path.exists(augFramePath) or not overwriteAugmented:
+                    continue
+
                 augFrame, augCoords = augmentFunc(frame, coords)
-                augFrameName = f"{fullCategory}{const.separator}{frameName}_{i}{const.separator}{augmentationName}"
 
                 augFrameMarks = {
                     const.image: extendName(augFrameName, extension),
@@ -119,8 +131,7 @@ def extract(ctg, ctgInfo, videosPath:Path.rawVideos, extractionPath=Path.origina
                     const.imageShape: augFrame.shape[:2]
                 }
 
-                cv2.imwrite(os.path.join(fullAugmentationPath, const.frames, extendName(augFrameName, extension)),
-                            augFrame)
+                cv2.imwrite(augFramePath, augFrame)
 
                 augMarks[augFrameName] = augFrameMarks
 
@@ -139,17 +150,47 @@ def extract(ctg, ctgInfo, videosPath:Path.rawVideos, extractionPath=Path.origina
     print(f"{Fore.GREEN}Added {total} pure frames and {totalAugs} augmentede frames in total {Style.RESET_ALL}")
 
 
-def extractCategories(videosPath:Path.rawVideos, summarizedPath, categoriesList, extractionPath=Path.original, subcategories=None):
-    subcategories = {} if subcategories is None else subcategories
+def extractCategories(videosPath=Path.rawVideos, summarizedPath=Path.summarizedRaw, categoriesList=None,
+                      framesLimit=None,
+                      augmentationsLimit=None, augmentationFunc=None, augmentationName="augmented",
+                      augmentationPath=None,
+                      extractionPath=Path.original, subcategories=None, parallel=True, threads=8,
+                      overwriteOriginal=False, overwriteAugmented=False):
 
     summarized = openJsonSafely(summarizedPath)
 
-    for category, categoryInfo in categoriesList.items():
-        curSubcategories = subcategories.get(category, list(categoryInfo.keys()))
+    subcategories = {} if subcategories is None else subcategories
+    categoriesList = list(summarized.keys()) if categoriesList is None else categoriesList
 
-        for subctg in curSubcategories:
-            subctgInfo = categoryInfo.get(subctg, {})
-            extract(subctg, subctgInfo, videosPath, extractionPath)
+    if parallel:
+        threads = min(threads, mp.cpu_count())
+    else:
+        threads = 1
+
+    with mp.Pool(threads) as pool:
+        for category in categoriesList:
+            categoryInfo = summarized[category]
+            curSubcategories = subcategories.get(category, list(categoryInfo.keys()))
+
+            for subctg in curSubcategories:
+                subctgInfo = categoryInfo.get(subctg, {})
+
+                pool.apply_async(
+                    extract,
+                    args=(subctg, subctgInfo),
+                    kwds={
+                        "videosPath": videosPath,
+                        "extractionPath": extractionPath,
+                        "extension": Extensions.jpg,
+                        "limit": framesLimit,
+                        "augmentFunc": augmentationFunc,
+                        "augmentations": augmentationsLimit,
+                        "augmentationName": augmentationName,
+                        "augmentationPath": augmentationPath,
+                        "overwriteOriginal": overwriteOriginal,
+                        "overwriteAugmented": overwriteAugmented
+                    }
+                )
 
 
 def main():
